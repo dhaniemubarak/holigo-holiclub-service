547575762dbe603d7a7e39c1e373c395cf5bddf9
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-11-01 10:03:46 +0700

    Assign user voucher

--- src/main/java/id/holigo/services/holigoholiclubservice/services/coupon/CouponServiceImpl.java
+++ src/main/java/id/holigo/services/holigoholiclubservice/services/coupon/CouponServiceImpl.java
@@ -3,13 +3,11 @@ package id.holigo.services.holigoholiclubservice.services.coupon;
 import id.holigo.services.common.model.CouponUserDto;
 import id.holigo.services.holigoholiclubservice.domain.UserClub;
 import id.holigo.services.holigoholiclubservice.repositories.UserClubRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class CouponServiceImpl implements CouponService {
 
@@ -29,7 +27,6 @@ public class CouponServiceImpl implements CouponService {
 
     @Override
     public void assignUserCoupon(UserClub userClub, CouponUserDto couponUser50, CouponUserDto couponUser100) {
-        log.info("assignUserCoupon is running....");
         ResponseEntity<HttpStatus> result50K = couponServiceFeignClient.createCoupon(couponUser50);
         ResponseEntity<HttpStatus> result100K = couponServiceFeignClient.createCoupon(couponUser100);
         if (result50K.getStatusCode().equals(HttpStatus.CREATED) && result100K.getStatusCode().equals(HttpStatus.CREATED)) {
